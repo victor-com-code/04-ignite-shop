9623ba76d6437f36a511b7192d3b09b874d13ff5
Victor-Eduardo <victorelalves@gmail.com>
2023-09-21 16:34:35 -0300

    Produto & Checkout - Checkout no Stripe

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -12,10 +12,16 @@ interface ProductProps {
     imageUrl: string
     price: string
     description: string
+    defaultPriceId: string
   }
 }
 
 export default function Product({ product }: ProductProps) {
+
+  function handleBuyProduct() {
+    console.log(product.defaultPriceId)
+  }
+
   const { isFallback } = useRouter()
 
   if (isFallback) {
@@ -34,7 +40,7 @@ export default function Product({ product }: ProductProps) {
 
         <p>{product.description}</p>
 
-        <button>Comprar agora</button>
+        <button onClick={handleBuyProduct}>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   )
@@ -71,6 +77,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async({ param
           currency: 'BRL',
         }).format(price.unit_amount as number / 100),
         description: product.description,
+        defaultPriceId: price.id,
       }
     },
     revalidate: 60 * 60 * 1, // 1 hour
